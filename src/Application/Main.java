59eae0caa8cf71128ce7dccfaee965c242bc0bef
Ravik Ferreira <ravikferreira.developer@gmail.com>
2022-10-13 13:16:08 -0300

    Seller entity class

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,12 +1,17 @@
 package Application;
 
 import Model.entities.Department;
+import Model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1,"BOOK" );
 
-        System.out.println(obj);
+        Seller seller =  new Seller(21, "BOB", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
