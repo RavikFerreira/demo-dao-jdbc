e1d8932c9ddeac6e5176264285fe6e1cf790109c
Ravik Ferreira <ravikferreira.developer@gmail.com>
2022-10-13 13:00:05 -0300

    Department entity class

---
+++ src/Application/Main.java
@@ -0,0 +1,12 @@
+package Application;
+
+import Model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1,"BOOK" );
+
+        System.out.println(obj);
+    }
+}
