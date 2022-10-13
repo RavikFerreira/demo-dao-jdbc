fc2f9048eb28dab44a343450e1a3c46b2450e12f
Ravik Ferreira <ravikferreira.developer@gmail.com>
2022-10-13 13:34:26 -0300

    SellerDao and DaoFactory

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,5 +1,7 @@
 package Application;
 
+import Model.dao.DaoFactory;
+import Model.dao.SellerDao;
 import Model.entities.Department;
 import Model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Main {
         Department obj = new Department(1,"BOOK" );
 
         Seller seller =  new Seller(21, "BOB", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
---
+++ src/Model/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package Model.dao;
+
+import Model.Dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+}
